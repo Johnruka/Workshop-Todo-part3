8ffbc4daa8b1bf6527b8ba2adaf60629d64f8515
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-10 10:22:09 +0200

    Created

---
+++ src/main/java/se/lexicon/model/Appointment.java
@@ -0,0 +1,78 @@
+package se.lexicon.model;
+
+import java.time.LocalDate;
+
+public class Appointment {
+
+    private final int id;
+    private String ChangeTires;
+    private LocalDate deadLine;
+    private boolean done;
+    private Person creator;
+
+    public Appointment(int id, String ChangeTires, LocalDate deadLine) {
+        this.id = id;
+        this.ChangeTires = ChangeTires;
+        this.deadLine = deadLine;
+
+    }
+
+    public Appointment(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getChangeTires() {
+        return ChangeTires;
+    }
+
+
+    public void setChangeTires(String changeTires) {
+        if (ChangeTires == null) throw new IllegalArgumentException("Change tires cannot be null");
+        this.ChangeTires = ChangeTires;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public boolean setDone(boolean done) {
+        return done;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public boolean setCreator(boolean Overdue) {
+        return Overdue;
+    }
+
+    public String getSummery(String summery) {
+        return summery;
+    }
+
+    public String getAppointmentInformation() {
+        String creatorInfo = creator != null ? " PersonId: " + creator.getId() : "-";
+        return String.format("Appointment{ id=%s, ChangeTires=%s, done=%b, creator=%s }", id, ChangeTires, done, creatorInfo);
+
+    }
+
+    public String isAvailable() {
+        String isAvailable = "isavailabe";
+        return isAvailable;
+    }
+
+    public Object setAssignee(String assignee) {
+        Object setAssignee = null;
+        return setAssignee;
+    }
+}
