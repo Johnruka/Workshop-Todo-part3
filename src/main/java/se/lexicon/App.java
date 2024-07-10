45803b301f7ced5ec991159ad669cf78b6bee392
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-10 11:40:48 +0200

    Created

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,19 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import se.lexicon.Dao.Person;
+import se.lexicon.model.PersonIdSequencer;
+
+public class App {
+    public static void main( String[] args ) {
+
+        int PersonIdSequencer = 0;
+        System.out.println(PersonIdSequencer);
+
+        Person person1 = new Person(0,"John","Smith","smith@gmail.com");
+
+        System.out.println(person1);
+
+
     }
 }
