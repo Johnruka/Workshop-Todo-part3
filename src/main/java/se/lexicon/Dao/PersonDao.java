569921f234ab07b901b64eb3c0445d913ee2d54b
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-11 21:31:24 +0200

    Modified

--- src/main/java/se/lexicon/Dao/PersonDao.java
+++ src/main/java/se/lexicon/Dao/PersonDao.java
@@ -1,13 +1,17 @@
 package se.lexicon.Dao;
 
+
 import se.lexicon.model.Person;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface PersonDao {
-    Person persist();
-    Person findById = null;
-    Person findByEmail = null;
-    Collection<Person> findAll();
-    Void remove();
+
+    Person persist(Person person);
+    Person findById(int id);
+    Person findByEmail(String email);
+    Person findUsername(String username);
+    List<Person> findAll();
+    void remove(int id);
+
 }
--- src/main/java/se/lexicon/Dao/PersonDaoCollection.java
+++ src/main/java/se/lexicon/Dao/PersonDaoCollection.java
@@ -1,25 +1,87 @@
 package se.lexicon.Dao;
 
 
-import java.util.Collection;
+import se.lexicon.Dao.Sequencer.PersonSequencer;
+import se.lexicon.model.AppUser;
+import se.lexicon.model.Person;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonDaoCollection implements PersonDao {
 
+    private final List<Person> people;
+
+    //create an object of SingleObject
+    private static PersonDaoCollection instance;
+
+    //make the constructor private so that this class cannot be instantiated
+    private PersonDaoCollection() {
+        people = new ArrayList<>();
+    }
+
+    //Get the only object available
+    public static PersonDaoCollection getInstance() {
+        if (instance == null) instance = new PersonDaoCollection();
+        return instance;
+
+
+
+
+    }
 
     @Override
-    public Person persist() {
+    public Person persist(Person person) {
+        if (person.getId() != 0) throw new IllegalStateException("Invalid state person.id was not 0 or null");
+        if (findByEmail(person.getEmail()) != null) {
+            throw new IllegalStateException("Email " + person.getEmail() + " is already taken");
+        }
+        person.setId(PersonSequencer.nextId());
+        people.add(person);
+        return person;
+    }
+
+    @Override
+    public Person findById(int id) {
+        for (Person person : people) {
+            if (person.getId() == id) {
+                return person;
+            }
+        }
         return null;
     }
 
     @Override
-    public Collection<Person> findAll() {
-        return List.of();
+    public Person findByEmail(String email) {
+        for (Person person : people) {
+            if (person.getEmail().equalsIgnoreCase(email)) {
+                return person;
+            }
+        }
+        return null;
     }
 
     @Override
-    public Void remove() {
+    public Person findUsername(String username) {
+        for (Person person : people) {
+            if (AppUser.getUsername().equalsIgnoreCase(username)) {
+                return person;
+            }
+        }
         return null;
+    }
+
+    @Override
+    public List<Person> findAll() {
+        return new ArrayList<>(people);
+    }
+
+    @Override
+    public void remove(int id) {
+        Person person = findById(id);
+        if (person != null) people.remove(person);
+
     }
 }
+
 
